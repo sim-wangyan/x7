c6baa761c2e9b35316d435da89f1a57be74d126f
simsevenx <cooperation@qq.com>
2022-03-08 17:20:22 +0800

    Rename UrlPara as UrlParamed (#64)

---
+++ x7-reyc/src/main/java/io/xream/x7/fallback/internal/monitor/FallbackMonitor.java
@@ -0,0 +1,13 @@
+package io.xream.x7.fallback.internal.monitor;
+
+/**
+ * @author Sim
+ */
+public class FallbackMonitor {
+
+//    private Counter orderCount;
+//
+//    private DistributionSummary amountSum;
+//
+//    private final MeterRegistry registry;
+}
